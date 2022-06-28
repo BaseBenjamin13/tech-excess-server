ec0854cd687b8f10eaddb0930f1cf89e3960ad25
BaseBenjamin13 <benmorgiewicz@gmail.com>
2022-06-27 22:34:45 -0400

    made model, seeded 3 monitors for testing

--- tech_excess/models.py
+++ tech_excess/models.py
@@ -1,3 +1,18 @@
+
 from django.db import models
 
 # Create your models here.
+
+
+class Monitors(models.Model):
+    title = models.CharField(max_length=50, default='no title')
+    brand = models.CharField(max_length=20, default='brand not found')
+    description = models.TextField()
+    price = models.IntegerField()
+    on_sale = models.BooleanField(default=False)
+    image_url = models.CharField(max_length=500, default='No image')
+
+    def __str__(self):
+        return self.title
+
+
---
+++ tech_excess/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('monitors/', views.MonitorsList.as_view(), name='monitors_list'),
+]
