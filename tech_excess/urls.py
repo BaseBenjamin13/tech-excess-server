ae39dda556e6498f4d19c30b0534d0a623351b8c
BaseBenjamin13 <benmorgiewicz@gmail.com>
2022-07-02 15:21:36 -0400

    made keyboard by id route

--- tech_excess/urls.py
+++ tech_excess/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('monitors/', views.MonitorsList.as_view(), name='monitors_list'),
     path('monitors/<int:pk>', views.MonitorDetail.as_view(), name='monitors_detail'),
     path('keyboards/', views.KeyboardsList.as_view(), name='keyboards_list'),
+    path('keyboards/<int:pk>', views.KeyboardDetail.as_view(), name='keyboards_detail'),
     path('mouses/', views.MousesList.as_view(), name='mouses_list'),
     path('headphones/', views.HeadPhonesList.as_view(), name='headphones_list'),
 ]
--- tech_excess/views.py
+++ tech_excess/views.py
@@ -8,7 +8,7 @@ from .models import Monitors, KeyBoard, Mouse, HeadPhone
 
 # Create your views here.
 
-
+#Monitors 
 class MonitorsList(generics.ListCreateAPIView):
     queryset = Monitors.objects.all()
     serializer_class = MonitorsSerializer
@@ -18,14 +18,23 @@ class MonitorDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = MonitorsSerializer
 
 
+#Keyboards
 class KeyboardsList(generics.ListCreateAPIView):
     queryset = KeyBoard.objects.all()
     serializer_class = KeyboardsSerializer
 
+class KeyboardDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = KeyBoard.objects.all()
+    serializer_class = KeyboardsSerializer
+
+
+#Mouses
 class MousesList(generics.ListCreateAPIView):
     queryset = Mouse.objects.all()
     serializer_class = MousesSerializer
 
+
+#Headphones
 class HeadPhonesList(generics.ListCreateAPIView):
     queryset = HeadPhone.objects.all()
     serializer_class = HeadPhonesSerializer
